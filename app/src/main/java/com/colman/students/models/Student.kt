65842e18a18d09c89a36c3d49adc766f05f4f215
Stav Barel <stavbarel2003@gmail.com>
2026-01-28 22:41:22 +0200

    Refactor Student data class by removing companion object constants

--- app/src/main/java/com/colman/students/models/Student.kt
+++ app/src/main/java/com/colman/students/models/Student.kt
@@ -6,12 +6,4 @@ data class Student(
     val phone: String,
     val address: String,
     var isChecked: Boolean = false
-) {
-    companion object {
-        const val ID_KEY = "id"
-        const val NAME_KEY = "name"
-        const val PHONE_KEY = "phone"
-        const val ADDRESS_KEY = "address"
-        const val IS_CHECKED_KEY = "isChecked"
-    }
-}
+)
