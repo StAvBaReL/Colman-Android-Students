53be9c153a3573aac129c8d9b1894eab1890f1bd
Stav Barel <stavbarel2003@gmail.com>
2026-01-28 22:41:16 +0200

    Add activity labels and update theme for improved navigation

--- app/src/main/java/com/colman/students/base/Constants.kt
+++ app/src/main/java/com/colman/students/base/Constants.kt
@@ -2,10 +2,6 @@ package com.colman.students.base
 
 import com.colman.students.models.Student
 
-typealias StudentsCompletion = (List<Student>) -> Unit
-typealias StudentCompletion = (Student) -> Unit
-typealias Completion = () -> Unit
-
 object Constants {
     const val STUDENT_ID_KEY = "student_id"
 }
